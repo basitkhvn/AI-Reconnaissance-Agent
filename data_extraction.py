d9a4c0cd58eda839ca0113f4a47a264b359b5912
Abdul Basit <basitkhan25m@gmail.com>
2026-03-06 00:16:29 +0500

    added the dnslookup function

--- data_extraction.py
+++ data_extraction.py
@@ -4,29 +4,48 @@ from langchain_core.messages import HumanMessage, AIMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import whois
+import dns.resolver
 
-class info(TypedDict):
+class whoisinfo(TypedDict):
     registrar: str
     creation_date: str
     expiry_date: str
     name_servers: list
-    error: str
+    error: str    
+     
 def formatdate(date):
      if isinstance(date,list):
           return str(date[0])
      elif isinstance(date,str):
           return str(date)
-def whoislookup(domain: str) -> info:
+def whoislookup(domain: str) -> whoisinfo:
         try:
             w=whois.whois(domain)
-            domain_info: info= {"registrar":w["registrar"],"creation_date": formatdate(w.get("creation_date")),"expiry_date": formatdate(w.get("expiration_date")),"name_servers": w["name_servers"]}
+            domain_info: whoisinfo= {"registrar":w["registrar"],"creation_date": formatdate(w.get("creation_date")),"expiry_date": formatdate(w.get("expiration_date")),"name_servers": w["name_servers"]}
             print(w["creation_date"])
             return domain_info
         except Exception as e:
              return {"error": str(e)}
-        
-    
+# fetch A and MX records   
+def dns_lookup(domain):
+     records={"A":[],"MX":[]}
+     try:
+          arecords=dns.resolver.resolve(domain,'A')
+          for rdata in arecords:
+               records["A"].append(str(rdata))
+     except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
+        pass 
+     try:
+           mxrecords=dns.resolver.resolve(domain,'MX')
+           for mxdata in mxrecords:
+                records["MX"].append(str(mxdata))
+     except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
+        pass # No MX records found
+     return records
+  
+domain="google.com"
+check=whoislookup(domain)
+print(f"Printing the whoislookup info: {check}")
+dnscheck=dns_lookup(domain)
+print(f"\nPrinting the dns info: {dnscheck}")
 
-
-check=whoislookup("google.oom")
-print(check)
